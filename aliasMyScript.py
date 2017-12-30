222ced4e4460bc049075a3e84fe8a9a0585021c7
unlockingc <624177150@qq.com>
2017-12-30 15:45:10 +0800

    2

--- aliasMyScript.py
+++ aliasMyScript.py
@@ -12,7 +12,7 @@ def addAlias():
     aliasDownload = "alias downloadMyDevEnv=\""+pwd+"/pullAndUpdate.py -p " + pwd + "\""
     aliasUpload = "alias uploadMyDevEnv=\""+pwd+"/collectAndPush.py -p " + pwd + "\""
     runCommand("sed -i '/^alias downloadMyDevEnv/d' ~/.myzshConf")
-    runCommand("sed -i '/^alias downloadMyDevEnv/d' ~/.myzshConf")
+    runCommand("sed -i '/^alias uploadMyDevEnv/d' ~/.myzshConf")
     runCommand("echo \""+aliasUpload+"\">> ~/.myzshConf")
     runCommand("echo \""+aliasDownload+"\">> ~/.myzshConf")
     runCommand("echo \""+aliasDownload+"\">> ~/.bash_aliases")
