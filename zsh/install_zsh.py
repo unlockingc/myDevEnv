191e8aee0f51bd461ace03cb5519d50275c45e8c
unlockingc <624177150@qq.com>
2017-12-30 15:41:42 +0800

    /home/user

--- zsh/install_zsh.py
+++ zsh/install_zsh.py
@@ -14,7 +14,8 @@ def installOhMyZsh():
     installSoftWare("curl wget")
     homePath = os.path.expanduser("~")
     newHomepath = homePath.replace("/","\/" )
-    runCommand("[ -f ~/.zshrc ] && sed -i 's/\/home\/*/"+newHomepath+"/g' ~/.zshrc || true")
+    #sed -n 's/\/home\/.*\/\.oh/${HOME}/p' ~/.zshrc
+    runCommand("[ -f ~/.zshrc ] && sed -i 's/\/home\/.*\/.oh/"+newHomepath+"\/.oh/g' ~/.zshrc || true")
     runCommand("sh -c \"$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)\"")
     #runCommand("chsh -s /bin/bash")   
 
@@ -27,7 +28,7 @@ def copyMyConf():
     runCommand("cp .zshrc ~/.zshrc")
     homePath = os.path.expanduser("~")
     newHomepath = homePath.replace("/","\/" )
-    runCommand("sed -i 's/\/home\/*/"+newHomepath+"/g' ~/.zshrc")
+    runCommand("sed -i 's/\/home\/.*\/.oh/"+newHomepath+"\/.oh/g' ~/.zshrc")
 
 def updateConfZsh():
     os.chdir("zsh")
